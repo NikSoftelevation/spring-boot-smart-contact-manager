d46b8b0cae8ab18cbfd61f75a94bd8fe194e6b4c
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-05-11 11:33:38 +0530

    Login System with Spring Security(Intro)

--- src/main/java/com/smartcontactmanager/config/Configuration.java
+++ src/main/java/com/smartcontactmanager/config/Configuration.java
@@ -1,4 +1,11 @@
 package com.smartcontactmanager.config;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 @org.springframework.context.annotation.Configuration
 public class Configuration {
-}
+
+    }
---
+++ src/main/java/com/smartcontactmanager/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.smartcontactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/index")
+    public String dashboard() {
+        return "normal/user_dashboard";
+    }
+}
