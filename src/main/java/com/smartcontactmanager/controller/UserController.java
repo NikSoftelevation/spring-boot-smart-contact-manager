76a7d6fe11f75562517e5826b43cf74bcf4081f0
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-05-12 17:51:18 +0530

    Designing Classic Add Contact Form(Smart Contact Manager)

--- src/main/java/com/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smartcontactmanager/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.smartcontactmanager.controller;
 
+import com.smartcontactmanager.domain.Contact;
 import com.smartcontactmanager.domain.User;
 import com.smartcontactmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -15,16 +18,36 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/index")
-    public String dashboard(Model model, Principal principal) {
+    //method for adding common data
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
 
         String userName = principal.getName();
         System.out.println(userName);
+
         //get the user using username(email)
+
         User user = userRepository.getUserByUserName(userName);
+
         System.out.println(user);
+
         model.addAttribute("user", user);
+    }
+
+    //dashboard home
+    @RequestMapping("/index")
+    public String dashboard(Model model, Principal principal) {
+
+        model.addAttribute("title", "User Dashboard");
 
         return "normal/user_dashboard";
+    }
+
+    //open add form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+        return "normal/add_contact_form";
     }
 }
