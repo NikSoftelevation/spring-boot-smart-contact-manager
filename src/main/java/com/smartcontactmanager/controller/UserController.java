8e21868a982533276455718b8b7822e1f03b8a25
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-05-15 13:03:03 +0530

    Uploading Image with contact details(Smart Contact Manager)

--- src/main/java/com/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/smartcontactmanager/controller/UserController.java
@@ -4,13 +4,17 @@ import com.smartcontactmanager.domain.Contact;
 import com.smartcontactmanager.domain.User;
 import com.smartcontactmanager.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -54,15 +58,39 @@ public class UserController {
 
     //processing contact form
     @PostMapping("/process-contact")
-    public String processContact(@ModelAttribute Contact contact, Principal principal) {
+    public String processContact(@ModelAttribute Contact contact, @RequestParam("profileImage") MultipartFile file, Principal principal) {
 
-        String name = principal.getName();
+        try {
+            String name = principal.getName();
 
-        User user = userRepository.getUserByUserName(name);
+            User user = userRepository.getUserByUserName(name);
 
-        user.getContacts().add(contact);
+            //processing and uploading file
 
-        userRepository.save(user);
+            if (file.isEmpty()) {
+                System.out.println("File is empty");
+
+            } else {
+                //upload the file to folder and update the name to contact
+
+                contact.setImage(file.getOriginalFilename());
+
+                File saveFile = new ClassPathResource("static/image").getFile();
+
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + file.getOriginalFilename());
+
+                Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Image is uploaded");
+            }
+
+            user.getContacts().add(contact);
+
+
+            userRepository.save(user);
+        } catch (Exception e) {
+            System.out.println("ERROR " + e.getMessage());
+            e.printStackTrace();
+        }
 
         return "normal/add_contact_form";
     }
