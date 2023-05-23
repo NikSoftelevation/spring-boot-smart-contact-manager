e700a62a468fc06d645cfb2264168f7b723628d0
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-05-23 16:46:57 +0530

    How to send Otp to email using|Forgot Password Module|(Smart Contact Manager)

--- src/main/java/com/smartcontactmanager/controller/ForgotController.java
+++ src/main/java/com/smartcontactmanager/controller/ForgotController.java
@@ -1,5 +1,9 @@
 package com.smartcontactmanager.controller;
 
+import com.smartcontactmanager.helper.Message;
+import com.smartcontactmanager.service.EmailService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +13,8 @@ import java.util.Random;
 
 @Controller
 public class ForgotController {
+    @Autowired
+    private EmailService emailService;
     Random random = new Random(1000);
 
     //email id form open handler
@@ -18,12 +24,27 @@ public class ForgotController {
     }
 
     @PostMapping("/send-otp")
-    public String sendOTP(@RequestParam("email") String email) {
+    public String sendOTP(@RequestParam("email") String email, HttpSession session) {
 
         /*generating 4 digits OTP*/
 
         int otp = random.nextInt(99999);
 
-        return "verify_otp";
+        /*Code for send otp to email*/
+
+        String subject = "OTP From SCM";
+        String message = "OTP=" + otp + "";
+        String to = email;
+
+        boolean flag = emailService.sendEmail(subject, message, to);
+
+        if (flag) {
+            session.setAttribute("otp", otp);
+            return "verify_otp";
+        } else {
+            session.setAttribute("message", new Message("Check your email id !!", ""));
+
+            return "forgot_email_form";
+        }
     }
 }
---
+++ src/main/java/com/smartcontactmanager/service/EmailService.java
@@ -0,0 +1,70 @@
+package com.smartcontactmanager.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+
+    public boolean sendEmail(String subject, String message, String to) {
+
+        boolean f = false;
+
+        String from = "nikhil.softelevation@gmail.com";
+
+        /*Variable for gmail*/
+        String host = "smtp.gmail.com";
+
+        /*get the system properties*/
+        Properties properties = System.getProperties();
+        System.out.println("PROPERTIES" + properties);
+
+        /*setting important information to properties object*/
+
+        /*host set*/
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+
+        /*Step 1: To get the session object*/
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("nikhil.softelevation@gmail.com", "welcome@123");
+            }
+        });
+        session.setDebug(true);
+        /*Step 2: Compose the message [text,multi media]*/
+        MimeMessage m = new MimeMessage(session);
+
+        try {
+            /*From email*/
+            m.setFrom(from);
+
+            /*Adding Recipient to message */
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            /*Adding Subject to message*/
+            m.setSubject(subject);
+
+            /*Adding Text to message*/
+            m.setText(message);
+
+            /*Send*/
+
+            /*Step 3: Send the message using Transport class*/
+            Transport.send(m);
+
+            System.out.println("Sent Success ................");
+            f = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getMessage();
+        }
+        return f;
+    }
+}
